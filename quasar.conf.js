5a90c7d840cd1056e686da593fb61630035bbde8
TahaBoulehmi <contact@btaha.com>
2019-07-31 16:46:03 +0200

    submit form + next question + showing the texts in the right font

--- quasar.conf.js
+++ quasar.conf.js
@@ -45,7 +45,7 @@ module.exports = function (ctx) {
         'QItem',
         'QItemSection',
         'QItemLabel',
-        'QCheckbox'
+        'QRadio'
       ],
 
       directives: [
--- src/pages/Index.vue
+++ src/pages/Index.vue
@@ -7,67 +7,30 @@
           <p style="color: red">Could you guess the name of this font?</p>
         </div>
         <div class="col-xs-12 col-sm-12 col-md-12 col-lg-12">
-          <strong>Lorum epsum lora epsu aloper epsis</strong>
+          <strong style="font-family: 'Montserrat'">Lorum epsum lora epsu aloper epsis</strong>
         </div>
       </div>
       <div class="col-2"></div>
     </div>
     <div class="col-2"></div>
     <div class="row col-8">
-      <q-item tag="label" v-ripple>
+
+      <q-item v-for="(font, index) in fonts" v-bind:key="index" tag="label" v-ripple>
         <q-item-section side top>
-          <q-checkbox v-model="check1" />
+          <q-radio :disabled="submittedForm" v-model="selectedFont" :val="index" />
         </q-item-section>
 
         <q-item-section>
-          <q-item-label>Notifications</q-item-label>
-          <q-item-label caption>
-            Notify me about updates to apps or games that I downloaded
-          </q-item-label>
-        </q-item-section>
-      </q-item>
-
-      <q-item tag="label" v-ripple>
-        <q-item-section side top>
-          <q-checkbox v-model="check2" />
-        </q-item-section>
-
-        <q-item-section>
-          <q-item-label>Sound</q-item-label>
-          <q-item-label caption>
-            Auto-update apps at anytime. Data charges may apply
-          </q-item-label>
-        </q-item-section>
-      </q-item>
-
-      <q-item tag="label" v-ripple>
-        <q-item-section side top>
-          <q-checkbox v-model="check3" />
-        </q-item-section>
-
-        <q-item-section>
-          <q-item-label>Auto-add widgets</q-item-label>
-          <q-item-label caption>
-            Automatically add home screen widgets and you have to
-          </q-item-label>
-        </q-item-section>
-      </q-item>
-
-      <q-item tag="label" v-ripple>
-        <q-item-section side top>
-          <q-checkbox v-model="check3" />
-        </q-item-section>
-
-        <q-item-section>
-          <q-item-label>Auto-add widgets</q-item-label>
-          <q-item-label caption>
-            Automatically add home screen widgets because you are
+          <q-item-label>{{font.name}}</q-item-label>
+          <q-item-label :style="{'font-family': (submittedForm ? font.name : null)}" caption>
+            This text will use the parent font. You should submit the form first.
           </q-item-label>
         </q-item-section>
       </q-item>
     </div>
     <div class="col-2"></div>
-    <q-btn label="Submit" color="primary" class="col-4" @click="signup()" />
+    <q-btn v-if="!submittedForm" label="Submit" color="primary" class="col-4" :disabled="selectedFont !== 0 && !selectedFont" @click="submit()" />
+    <q-btn v-else label="Next" color="secondary" class="col-4" @click="nextQuestion()" />
   </q-page>
 </template>
 
@@ -79,9 +42,37 @@ export default {
   name: 'PageIndex',
   data () {
     return {
-      check1: true,
-      check2: false,
-      check3: false
+      fonts: [
+        {
+          name: 'Roboto',
+          url: 'xxx'
+        },
+        {
+          name: 'Lato',
+          url: 'xxx'
+        },
+        {
+          name: 'Farro',
+          url: 'xxx'
+        },
+        {
+          name: 'Fira Code',
+          url: 'xxx'
+        }
+      ],
+      selectedFont: undefined,
+      rightFont: 2,
+      submittedForm: false
+    }
+  },
+  methods: {
+    submit () {
+      this.submittedForm = true;
+      (this.selectedFont && this.rightFont && this.selectedFont === this.rightFont) ? alert(true) : alert(false)
+    },
+    nextQuestion () {
+      this.selectedFont = undefined
+      this.submittedForm = false
     }
   }
 }
